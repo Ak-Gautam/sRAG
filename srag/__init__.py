3d94ca834ffea949aa33fd801fb618d2879e4e25
Akshay Gautam <61274514+Gautam-flash@users.noreply.github.com>
2024-10-07 10:42:01 +0530

    bug fixes.

--- srag/__init__.py
+++ srag/__init__.py
@@ -1,14 +1,14 @@
-from srag.document_loader import DocumentLoader, Document
-from srag.chunk_node import get_chunk_splitter, Node, ChunkSplitter, TokenChunkSplitter, SentenceChunkSplitterWithOverlap, ParagraphChunkSplitter
-from srag.embeddings import Embeddings
-from srag.vector_store import VectorStore
-from srag.llm import LLM
-from srag.prompt_manager import PromptManager
-from srag.rag_pipeline import RAGPipeline
-from srag.data_generator import DataGenerator
+from .document_loader import DocumentLoader, Document
+from .chunk_node import get_chunk_splitter, Node, ChunkSplitter, TokenChunkSplitter, SentenceChunkSplitterWithOverlap, ParagraphChunkSplitter
+from .embeddings import Embeddings
+from .vector_store import VectorStore
+from .llm import LLM
+from .prompt_manager import PromptManager
+from .rag_pipeline import RAGPipeline
+from .data_generator import DataGenerator
 
 __all__ = [
-    "DocumentLoader", "Document",
+    "DocumentLoader", "Document", 
     "get_chunk_splitter", "Node", "ChunkSplitter", "TokenChunkSplitter", "SentenceChunkSplitterWithOverlap", "ParagraphChunkSplitter",
     "Embeddings",
     "VectorStore",
